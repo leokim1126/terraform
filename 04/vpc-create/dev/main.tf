b0e8b22ace8fe6a2ef795a9f095e5afa1c678ebf
pancake1126 <leokim1126@naver.com>
2025-12-18 22:14:44 +0900

    sixth commit

---
+++ 04/vpc-create/dev/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+module "my_vpc" {
+  source = "../modules/vpc"
+  
+}
+
+module "my_ec2" {
+  source = "../modules/ec2"
+
+  subnet_id = module.my_vpc.subnet_id 
+}
+
